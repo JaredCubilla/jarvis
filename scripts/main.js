03e7012211d810d014f8fc33c0efded4bc6272cf
JaredCubilla <deraj.allibuc@gmail.com>
2014-09-18 21:06:43 -0400

    oh wait no it is an array

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 var recognition = new webkitSpeechRecognition();
 recognition.onresult = function(event) {
-    console.log(event.results);
+    console.log(event.results[0][0].transcript);
 }
 
 document.getElementById('microphone-button').addEventListener('click', function(event) {
