1112a642a237cca4571e9944a2ee4481ecd18376
JaredCubilla <deraj.allibuc@gmail.com>
2014-09-18 21:14:49 -0400

    changed position of parse

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 var recognition = new webkitSpeechRecognition();
-recognition.onresult = function(event) {
-    $("#test").html("You just said \"" + event.results[0][0].transcript + "\"");
-    parse(event.results[0][0].transcript);
-}
 
 function parse(str) {
 	console.log(str);
+}
+
+recognition.onresult = function(event) {
+    $("#test").html("You just said \"" + event.results[0][0].transcript + "\"");
+    parse(event.results[0][0].transcript);
 }
 
 document.getElementById('microphone-button').addEventListener('click', function(event) {
