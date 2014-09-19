2e4d7684a7fe04c16c34ae1379995c95e36367f9
JaredCubilla <deraj.allibuc@gmail.com>
2014-09-18 21:13:56 -0400

    added parse function

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
 var recognition = new webkitSpeechRecognition();
 recognition.onresult = function(event) {
     $("#test").html("You just said \"" + event.results[0][0].transcript + "\"");
+    parse(event.results[0][0].transcript);
+}
+
+function parse(str) {
+	console.log(str);
 }
 
 document.getElementById('microphone-button').addEventListener('click', function(event) {
