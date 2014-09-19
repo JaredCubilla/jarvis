8b0c833ecab17075e5ae2783ca95312f08e89523
JaredCubilla <deraj.allibuc@gmail.com>
2014-09-18 21:10:32 -0400

    added you just said

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 var recognition = new webkitSpeechRecognition();
 recognition.onresult = function(event) {
-    $("#test").html(event.results[0][0].transcript);
+    $("#test").html("You just said \"" + event.results[0][0].transcript = "\"");
 }
 
 document.getElementById('microphone-button').addEventListener('click', function(event) {
