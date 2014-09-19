9e30b11ac6e1217b8923d1e253aab0115c77a5dd
JaredCubilla <deraj.allibuc@gmail.com>
2014-09-18 20:59:33 -0400

    indents, microphone button

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-    recognition.onstart = function() {console.log('pizza')};
-    recognition.onresult = function(event) 
-    { 
-        console.log(event);
-    }
-    recognition.start();
+var recognition = new webkitSpeechRecognition();
+recognition.onstart = function() {console.log('pizza')};
+recognition.onresult = function(event) 
+{ 
+    console.log(event);
+}
+
+document.getElementById('microphone-button').addEventListener('click', function(event) {
+	recognition.start();
+});
